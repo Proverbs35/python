ffbed21ec0b4259b948c1eafe160a9a0961d7774
Proverbs35 <ncancel@fingerlakes.edu>
2025-03-06 22:20:05 -0500

    Update for_loop_list.py

--- for_loop_list.py
+++ for_loop_list.py
@@ -17,18 +17,20 @@
 def main():
     doggo_names = get_doggo_names()
     output_names(doggo_names)
-
+    
 def get_doggo_names():
     name = ''
     names = []
     while name != 'DONE':
         name = input('Name of doggo: ')
-        if name != 'DONE':
-    
+        if name != 'DONE': 
+           \n names.append(name)    
     return names
 
 def output_names(doggo_names):
-    print('f{name} is awesome.')
+    print()
+    for name in doggo_names:
+        print(f'{name} is awesome.')
 
 main()
     
