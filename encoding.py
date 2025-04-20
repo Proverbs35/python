979ebffdd58edce0bf2465321c62480cfc2104d1
Proverbs35 <ncancel@fingerlakes.edu>
2025-04-20 18:52:19 -0400

    Add files via upload

---
+++ encoding.py
@@ -0,0 +1,32 @@
+import base64 
+
+# FILE NAME - encoding.py
+
+# NAME - 
+# DATE - 
+# DESCRIPTION - 
+
+  
+
+
+
+def main():
+    encode_data()
+
+def encode_data():
+    # OPEN FILE
+    
+    
+    
+    # GET DATA
+    
+    
+    
+    # OUTPUT ENCODED DATA
+
+
+
+
+
+
+main()
